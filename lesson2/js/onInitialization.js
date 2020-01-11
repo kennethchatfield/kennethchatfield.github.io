e83a0fed8b50bf37eb946ef3f21147c8228a9c09
kenneth chatfield <chatfield.kenny@gmail.com>
2020-01-10 23:53:53 -0700

    style automation 2.0

--- lesson2/js/onInitialization.js
+++ lesson2/js/onInitialization.js
@@ -2,22 +2,38 @@ const hiddenByDefault = [
     "principle-content-container",
     "principle-description",
     "principle-example-container"
-  ];
-  
-  function hideElement(element){
-    element.style.display = 'none';
-  
-  }
-  
-  function initHiddenStyles(className){
-    const elementsToHide = document.getElementsByClassName(className);
-    for (let i = 0; i < elementsToHide.length; i++) {
-      hideElement(elementsToHide[i])
+];
+
+const stylesByClass = {
+    "principle-title": {
+        "box-shadow": "4px 4px 4px rgb(192, 192, 192)"
     }
-  }
+}
   
-  function onInitialization(){
-    hiddenByDefault.map(initHiddenStyles);
-  }
+function hideElement(className, element){
+    console.log('hideElement',{className, element})
+    if(hiddenByDefault.includes(className)){
+     element.style.display = 'none';
+    }
+}
+function addStylesByClass( className, element ) {
+    if(stylesByClass[className]){
+    Object.keys(stylesByClass[className]).map(styleAttr => {
+        element.style[styleAttr] = stylesByClass[className][styleAttr]
+    })
+    }
+}
+  
+function initStyles(className){
+    const elements = document.getElementsByClassName(className);
+    for (let i = 0; i < elements.length; i++) {
+        hideElement(className,elements[i]);
+        addStylesByClass(className,elements[i]);
+    }
+}
+  
+function onInitialization(){
+    [...hiddenByDefault, ...Object.keys(stylesByClass)].map(initStyles);
+}
   
   window.onload = onInitialization;
