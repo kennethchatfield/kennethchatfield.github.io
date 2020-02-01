a1f15873728953fd6bf05186b21af96c9b963d37
kenneth chatfield <chatfield.kenny@gmail.com>
2020-01-31 22:18:36 -0700

    lesson4 draft2

--- lesson4/js/toggle_expand.js
+++ lesson4/js/toggle_expand.js
@@ -1,14 +1,4 @@
 
 function toggleExpand(){
-    const menuToggleElement = document.getElementById("nav-menu-toggle-label");
-    const classNameString = menuToggleElement.className;
-    let classNames = classNameString === "" ? [] : classNameString.split(" ");
-
-    if( classNames.includes("expand")){
-        classNames = classNames.filter( className => className !== "expand");
-    } else {
-        classNames.push( "expand" );
-    }
-
-    menuToggleElement.className = classNames.join(" ");
+    document.querySelector('#nav-menu-toggle-label').classList.toggle("expand")
 }
