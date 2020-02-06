b595f0fd2ba5ad9803bd302081ca9dffdea01e5b
kenneth chatfield <chatfield.kenny@gmail.com>
2020-02-06 00:18:56 -0700

    preston page commit 1

---
+++ lesson5/js/current_date.js
@@ -0,0 +1,45 @@
+let currentDateObject = new Date();
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+const dayString = days[ currentDateObject.getDay() ];
+const dateString = currentDateObject.getDate();
+const monthString = months[ currentDateObject.getMonth() ];
+const yearString = currentDateObject.getUTCFullYear();
+
+
+const formattedDateString = [
+    dayString + ",",
+    dateString,
+    monthString,
+    yearString
+].join(" ")
+
+console.log(formattedDateString)
+
+// console.log('Last Modified', new Date( document.lastModified).getUTCMonth)
+
+document.getElementById("current-date").textContent = formattedDateString;
